fdf94a4d464487208dbf36e3c3d9b8cdd11e1e9e
EC2 Default User <ec2-user@ip-172-31-34-217.ap-northeast-1.compute.internal>
2019-10-08 10:57:59 +0000

    認証メールのurl設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+   config.action_mailer.default_url_options = { protocol: 'https', host: 'https://b35020dc2b2741568f45743d6403e600.vfs.cloud9.ap-northeast-1.amazonaws.com' }
 end
